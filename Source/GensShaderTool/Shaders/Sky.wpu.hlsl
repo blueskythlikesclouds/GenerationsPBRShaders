3e24caf587ddf72087dfa767f8dbff529a0d181c
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-02-19 12:24:10 +0300

    Change how sky shader unpacks HDR data.
    SIGNIFICANTLY higher texture quality. This is how default IBL textures do it.

--- Source/GensShaderTool/Shaders/Sky.wpu.hlsl
+++ Source/GensShaderTool/Shaders/Sky.wpu.hlsl
@@ -1,8 +1,6 @@
 #include "../global.psparam.hlsl"
 #include "Functions.hlsl"
 
-float4 Scale : register(c150);
-
 sampler2D diffuseSampler : register(s0);
 sampler2D transparencySampler : register(s1);
 
@@ -10,7 +8,8 @@ float4 main(in float2 vPos : VPOS, in float4 texCoord0 : TEXCOORD0, in float4 te
 {
     float4 diffuse = tex2D(diffuseSampler, texCoord0.xy);
 
-    diffuse.rgb = UnpackHDRCustom(diffuse.rgb, Scale.x) * g_BackGroundScale.x;
+    diffuse.rgb = UnpackHDR(diffuse) * g_BackGroundScale.x;
+    diffuse.a = 1;
 
 #if defined(HasTransparency) && HasTransparency
     diffuse.a *= tex2D(transparencySampler, texCoord0.xy).x;
