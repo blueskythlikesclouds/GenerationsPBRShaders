9db6eafa6c2d59add97acbe1996e6de5bc82c995
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-04-23 21:25:15 +0300

    Fix RLR glitching out from certain angles.

--- Source/GensShaderTool/Shaders/Filter/ConvolutionFilter.wpu.hlsl
+++ Source/GensShaderTool/Shaders/Filter/ConvolutionFilter.wpu.hlsl
@@ -9,8 +9,8 @@ float4 main(in float2 texCoord : TEXCOORD) : COLOR
 {
     float4 color = 0.0;
     float2 off1 = 1.3333333333333333 * g_Direction.xy;
-    color += tex2Dlod(s0, float4(texCoord * g_Param.z,                     0, g_Param.w)) * 0.29411764705882354;
-    color += tex2Dlod(s0, float4(texCoord * g_Param.z + off1 * g_Param.xy, 0, g_Param.w)) * 0.35294117647058826;
-    color += tex2Dlod(s0, float4(texCoord * g_Param.z - off1 * g_Param.xy, 0, g_Param.w)) * 0.35294117647058826;
+    color += max(0, tex2Dlod(s0, float4(texCoord * g_Param.z,                     0, g_Param.w)) * 0.29411764705882354);
+    color += max(0, tex2Dlod(s0, float4(texCoord * g_Param.z + off1 * g_Param.xy, 0, g_Param.w)) * 0.35294117647058826);
+    color += max(0, tex2Dlod(s0, float4(texCoord * g_Param.z - off1 * g_Param.xy, 0, g_Param.w)) * 0.35294117647058826);
     return color;
 }
