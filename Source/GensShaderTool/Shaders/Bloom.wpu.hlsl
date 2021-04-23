0faf102ea94232761c54c783ab5ae750e6427ff4
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-04-23 16:05:28 +0300

    Fix Sonic Forces bloom shader.

--- Source/GensShaderTool/Shaders/Bloom.wpu.hlsl
+++ Source/GensShaderTool/Shaders/Bloom.wpu.hlsl
@@ -5,5 +5,6 @@ float4 main(float4 texCoord : TEXCOORD0) : COLOR
 {
     float3 color = clamp(tex2Dlod(sampDif0, float4(texCoord.xy, 0, 0)).rgb, 0, 65504);
     float maxCoeff = max(0.001, max(color.r, max(color.g, color.b)));
-    return float4(color / maxCoeff * saturate((maxCoeff - g_BloomStar_Param1.x) / g_BloomStar_Param1.y), 1);
+    float factor = saturate((maxCoeff - g_BloomStar_Param1.x) / (g_BloomStar_Param1.y - g_BloomStar_Param1.x));
+    return float4(color / maxCoeff * factor, 1);
 }
