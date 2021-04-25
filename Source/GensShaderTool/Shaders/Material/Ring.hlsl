8bb3448a83c1f61eae12c2c7168dd1129653627b
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2021-04-25 15:49:27 +0300

    Add specular & normal to Ring shader.

--- Source/GensShaderTool/Shaders/Material/Ring.hlsl
+++ Source/GensShaderTool/Shaders/Material/Ring.hlsl
@@ -9,11 +9,10 @@
 
 #include "Default.hlsl"
 
-#undef DECLARATION_TYPE
-#define DECLARATION_TYPE    DefaultDeclaration
-
 sampler2D diffuseSampler : register(s0);
 sampler2D emissionSampler : register(s1);
+sampler2D specularSampler : register(s2);
+sampler2D normalSampler : register(s3);
 
 float4 PBRFactor : register(c150);
 float4 Blend : register(c151);
@@ -25,7 +24,22 @@ float4 GetDiffuse(DECLARATION_TYPE input, Material material)
 
 float4 GetSpecular(DECLARATION_TYPE input)
 {
+#if defined(HasSpecular) && HasSpecular
+    float4 specular = tex2D(specularSampler, UV(1));
+    return float4(specular.x * 0.25, specular.yzw);
+#else
     return float4(PBRFactor.xy, 1, 1);
+#endif
+}
+
+float3 GetNormal(DECLARATION_TYPE input, float3x3 tangentToWorldMatrix)
+{
+    float4 normal = tex2D(normalSampler, UV(2));
+
+    normal.xy = normal.xy * 2 - 1;
+    normal.z = sqrt(1 - saturate(dot(normal.xy, normal.xy)));
+
+    return mul(tangentToWorldMatrix, normal.xyz);
 }
 
 void PostProcessMaterial(DECLARATION_TYPE input, inout Material material)
