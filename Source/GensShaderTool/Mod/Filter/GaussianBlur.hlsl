da697620fbcd502a9f3e722bb46e530bb6ec8c2b
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2022-09-12 22:28:46 +0300

    Fix RLR glitching out at certain angles.

--- Source/GensShaderTool/Mod/Filter/GaussianBlur.hlsl
+++ Source/GensShaderTool/Mod/Filter/GaussianBlur.hlsl
@@ -11,7 +11,7 @@ Texture2D<float4> g_SourceTexture : register(t4);
 
 float4 main(in float4 unused : SV_POSITION, in float4 texCoord : TEXCOORD0) : SV_TARGET
 {
-    return (g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale, g_Level) * 0.29411764705882354) +
-        (g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale + g_Offset, g_Level) * 0.35294117647058826) +
-        (g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale - g_Offset, g_Level) * 0.35294117647058826);
+    return max(0, g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale, g_Level) * 0.29411764705882354) +
+        max(0, g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale + g_Offset, g_Level) * 0.35294117647058826) +
+        max(0, g_SourceTexture.SampleLevel(g_LinearClampSampler, texCoord.xy * g_Scale - g_Offset, g_Level) * 0.35294117647058826);
 }
